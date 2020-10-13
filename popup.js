539818df338876f374123d7b459404f76079f94b
furyu-isomata <isomata.satsuki@furyu.jp>
2020-10-13 14:35:50 +0900

    基本の挙動OK

--- popup.js
+++ popup.js
@@ -2,13 +2,9 @@
 new Vue({
     el: "#app",
     data: {
-        wordCount: 0,
         textCount: ""
     },
     methods: {
-        hoge(){
-            const target = document.getElementsByClassName("mem-name")
-            console.log(target);
         }
     },
     mounted(){
